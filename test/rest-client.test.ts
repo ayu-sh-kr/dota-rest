8df8a5b46a25b44879670c7347e027b93b48adf2
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-04-10 15:50:13 +0530

    Refactor RestClient tests for improved readability and consistency

--- test/rest-client.test.ts
+++ test/rest-client.test.ts
@@ -1,117 +1,117 @@
 import {RestClient} from "@dota/index.ts";
 
 describe('RestClient', () => {
-    beforeEach(() => {
-        global.fetch = jest.fn();
+  beforeEach(() => {
+    global.fetch = jest.fn();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should create a RestClient with the correct baseUrl, headers, and timeout', () => {
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .defaultHeaders({'Authorization': 'Bearer token'})
+      .timeout(5000)
+      .build();
+
+    expect(client).toHaveProperty("config")
+    expect(client).toMatchObject({
+      "config": {"baseUrl": "https://api.example.com", "headers": {"Authorization": "Bearer token"}, "timout": 5000}
+    })
+  });
+
+  it('should make a GET request and return the response entity', async () => {
+    const mockResponse = new Response(JSON.stringify({key: 'value'}), {
+      status: 200,
+      headers: {'Content-Type': 'application/json'}
     });
+    (global.fetch as jest.Mock).mockResolvedValueOnce(mockResponse);
 
-    afterEach(() => {
-        jest.resetAllMocks();
-    });
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .defaultHeaders({'Authorization': 'Bearer token'})
+      .timeout(5000)
+      .build();
 
-    it('should create a RestClient with the correct baseUrl, headers, and timeout', () => {
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .defaultHeaders({ 'Authorization': 'Bearer token' })
-            .timeout(5000)
-            .build();
+    const response = await client.get<{ key: string }>()
+      .uri('/test')
+      .retrieve()
+      .toEntity();
 
-        expect(client).toHaveProperty("config")
-        expect(client).toMatchObject({
-            "config": {"baseUrl": "https://api.example.com", "headers": {"Authorization": "Bearer token"}, "timout": 5000}
-        })
-    });
+    expect(response.status).toBe(200);
+    expect(response.data).toEqual({key: 'value'});
+  });
 
-    it('should make a GET request and return the response entity', async () => {
-        const mockResponse = new Response(JSON.stringify({ key: 'value' }), {
-            status: 200,
-            headers: { 'Content-Type': 'application/json' }
-        });
-        (global.fetch as jest.Mock).mockResolvedValueOnce(mockResponse);
+  it('should create a RestRequestMaker with method GET, header, baseUrl and timeout', () => {
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .timeout(5000)
+      .build();
 
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .defaultHeaders({ 'Authorization': 'Bearer token' })
-            .timeout(5000)
-            .build();
+    const requestBuilder = client.get<{ key: string }>()
 
-        const response = await client.get<{ key: string }>()
-            .uri('/test')
-            .retrieve()
-            .toEntity();
-
-        expect(response.status).toBe(200);
-        expect(response.data).toEqual({ key: 'value' });
-    });
-
-    it('should create a RestRequestMaker with method GET, header, baseUrl and timeout', () => {
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .timeout(5000)
-            .build();
-
-        const requestMaker = client.get<{ key: string }>()
-
-        expect(requestMaker).toHaveProperty("method", 'GET');
-        expect(requestMaker).toHaveProperty("baseUri", 'https://api.example.com');
-        expect(requestMaker).toHaveProperty("_timeout", 5000);
-    });
+    expect(requestBuilder).toHaveProperty("method", 'GET');
+    expect(requestBuilder).toHaveProperty("_baseUri", 'https://api.example.com');
+    expect(requestBuilder).toHaveProperty("_timeout", 5000);
+  });
 
 
-    it('should create a RestRequestMaker with method PATCH, header, baseUrl and timeout', () => {
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .timeout(5000)
-            .build();
+  it('should create a RestRequestMaker with method PATCH, header, baseUrl and timeout', () => {
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .timeout(5000)
+      .build();
 
-        const requestMaker = client.patch<{ key: string }>();
+    const requestBuilder = client.patch<{ key: string }>();
 
-        expect(requestMaker).toHaveProperty("method", 'PATCH');
-        expect(requestMaker).toHaveProperty("baseUri", 'https://api.example.com');
-        expect(requestMaker).toHaveProperty("_timeout", 5000);
-        expect(requestMaker).toHaveProperty("_headers", {"Content-type": "application/json"});
-    });
+    expect(requestBuilder).toHaveProperty("method", 'PATCH');
+    expect(requestBuilder).toHaveProperty("_baseUri", 'https://api.example.com');
+    expect(requestBuilder).toHaveProperty("_timeout", 5000);
+    expect(requestBuilder).toHaveProperty("_defaultHeaders", {"Content-type": "application/json"});
+  });
 
-    it('should create a RestRequestMaker with method POST, header, baseUrl and timeout', () => {
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .timeout(5000)
-            .build();
+  it('should create a RestRequestMaker with method POST, header, baseUrl and timeout', () => {
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .timeout(5000)
+      .build();
 
-        const requestMaker = client.post();
+    const requestBuilder = client.post();
 
-        expect(requestMaker).toHaveProperty("method", 'POST')
-        expect(requestMaker).toHaveProperty("baseUri", 'https://api.example.com')
-        expect(requestMaker).toHaveProperty("_timeout", 5000);
-        expect(requestMaker).toHaveProperty("_headers", {"Content-type": "application/json"})
-    });
+    expect(requestBuilder).toHaveProperty("method", 'POST')
+    expect(requestBuilder).toHaveProperty("_baseUri", 'https://api.example.com')
+    expect(requestBuilder).toHaveProperty("_timeout", 5000);
+    expect(requestBuilder).toHaveProperty("_defaultHeaders", {"Content-type": "application/json"})
+  });
 
-    it('should create a RestRequestMaker with method PUT, header, baseUrl and timeout', () => {
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .timeout(5000)
-            .build();
+  it('should create a RestRequestMaker with method PUT, header, baseUrl and timeout', () => {
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .timeout(5000)
+      .build();
 
-        const requestMaker = client.put<{ key: string }>();
+    const requestBuilder = client.put<{ key: string }>();
 
-        expect(requestMaker).toHaveProperty("method", 'PUT')
-        expect(requestMaker).toHaveProperty("baseUri", 'https://api.example.com')
-        expect(requestMaker).toHaveProperty("_timeout", 5000);
-        expect(requestMaker).toHaveProperty("_headers", {"Content-type": "application/json"})
-    });
+    expect(requestBuilder).toHaveProperty("method", 'PUT')
+    expect(requestBuilder).toHaveProperty("_baseUri", 'https://api.example.com')
+    expect(requestBuilder).toHaveProperty("_timeout", 5000);
+    expect(requestBuilder).toHaveProperty("_defaultHeaders", {"Content-type": "application/json"})
+  });
 
-    it('should create a RestRequestMaker with method DELETE, header, baseUrl and timeout', () => {
-        const client = RestClient.create()
-            .baseUrl('https://api.example.com')
-            .timeout(5000)
-            .build();
+  it('should create a RestRequestMaker with method DELETE, header, baseUrl and timeout', () => {
+    const client = RestClient.builder()
+      .baseUrl('https://api.example.com')
+      .timeout(5000)
+      .build();
 
-        const requestMaker = client.delete<{ key: string }>();
+    const requestBuilder = client.delete<{ key: string }>();
 
-        expect(requestMaker).toHaveProperty("method", 'DELETE')
-        expect(requestMaker).toHaveProperty("baseUri", 'https://api.example.com')
-        expect(requestMaker).toHaveProperty("_timeout", 5000);
-        expect(requestMaker).toHaveProperty("_headers", {})
-    });
+    expect(requestBuilder).toHaveProperty("method", 'DELETE')
+    expect(requestBuilder).toHaveProperty("_baseUri", 'https://api.example.com')
+    expect(requestBuilder).toHaveProperty("_timeout", 5000);
+    expect(requestBuilder).toHaveProperty("_defaultHeaders", {})
+  });
 
 });
